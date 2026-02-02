f048dba20ee981bd731b8ed863f8c6a15ce4291f
Coding <150249195+Dammy-wise@users.noreply.github.com>
2026-02-02 07:37:10 +0100

    commit1

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 export default function Hero() {
+
+   const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  
   return (
     <section id="home" className="hero">
       <div className="hero-inner">
@@ -8,9 +16,16 @@ export default function Hero() {
          <div className="hero-text1">
           <h1 className="hero-name">
             <span className="first-name">Oluwadamilola</span>
+            <span className="middle-name">Stephen</span>
             <br />
             <span className="last-name">Otunla</span>
           </h1>
+          <button
+                className="mywork"
+                onClick={() => scrollToSection("projects")}
+              >
+                My Work
+              </button>
           <span className="hero-sub">
             <p className="hero-sub-right-fixed">
               Web Developer 
@@ -18,7 +33,7 @@ export default function Hero() {
             <p className="hero-sub-right2">
               <span>I handle the development, deployment</span>
               <br />
-              <span>and maintenance of your Website, start to finish.</span>
+              <span>and maintenance of Games, start to finish.</span>
             </p>
           </span>
           </div>
